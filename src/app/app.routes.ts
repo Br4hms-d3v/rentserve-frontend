89ef35863863e8e31bec55ae8d8422cac28fcb64
unknown <user.name>
2025-06-15 20:17:44 +0200

    Home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {HomeComponent} from './features/home/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'rent-serve', pathMatch: 'full'},
+  {path: 'rent-serve', component: HomeComponent}
+];
