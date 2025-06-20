c9bac0f9bac0bebdf155e0174152bbccae407686
unknown <user.name>
2025-06-20 19:23:54 +0200

    Nav with change color and buttons

--- src/app/shared/layouts/nav/nav.component.ts
+++ src/app/shared/layouts/nav/nav.component.ts
@@ -1,16 +1,29 @@
-import {Component} from '@angular/core';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import {Component, OnInit} from '@angular/core';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatIcon} from '@angular/material/icon';
+import {MatFabButton} from '@angular/material/button';
+import {NgClass} from '@angular/common';
+import {ThemeService} from '../../../core/services/theme.service';
+
 
 @Component({
   selector: 'app-nav',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbar, MatIcon, MatFabButton, NgClass],
   templateUrl: './nav.component.html',
   styleUrl:
     './nav.component.scss'
 })
 
-export class NavComponent {
+export class NavComponent implements OnInit {
+
+  isDarkMode = false;
+
+  constructor(private themeService: ThemeService) {
+  }
+
+  ngOnInit(): void {
+    this.isDarkMode = this.themeService.isDarkMode();
+    this.themeService.darkMode$.subscribe(mode => this.isDarkMode = mode);
+  }
 
 }
