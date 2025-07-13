9706ec57351926942ec1d43c5a4d7b50e0cd88b7
Brahms <bramso.brahms@gmail.com>
2025-07-14 00:53:11 +0200

    Add condition to display button when the user is really connect

--- src/app/shared/layouts/nav/nav.component.ts
+++ src/app/shared/layouts/nav/nav.component.ts
@@ -1,16 +1,19 @@
 import {Component, OnInit} from '@angular/core';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MatIcon} from '@angular/material/icon';
-import {MatFabButton} from '@angular/material/button';
-import {NgClass} from '@angular/common';
+import {MatButtonModule, MatFabButton} from '@angular/material/button';
 import {ThemeService} from '../../../core/services/theme.service';
 import {MatDialog} from '@angular/material/dialog';
 import {LoginComponent} from '../../../features/auth/login/login.component';
+import {NgClass} from '@angular/common';
+import {AuthService} from '../../../core/services/auth.service';
+import {MatMenuModule} from '@angular/material/menu';
+import {Router} from '@angular/router';
 
 
 @Component({
   selector: 'app-nav',
-  imports: [MatToolbar, MatIcon, MatFabButton, NgClass],
+  imports: [MatToolbar, MatIcon, MatFabButton, NgClass, MatButtonModule, MatMenuModule],
   templateUrl: './nav.component.html',
   styleUrl:
     './nav.component.scss'
@@ -20,7 +23,12 @@ export class NavComponent implements OnInit {
 
   isDarkMode = false;
 
-  constructor(private themeService: ThemeService, private dialog: MatDialog) {
+  constructor(
+    private themeService: ThemeService,
+    private dialog: MatDialog,
+    private authService: AuthService,
+    private readonly _router: Router,
+  ) {
   }
 
   ngOnInit(): void {
@@ -35,6 +43,17 @@ export class NavComponent implements OnInit {
       height: '400px',
       data: {}
     });
+  }
+
+  // Authenticated user for display something
+  isAuthenticated() {
+    return this.authService.isAuthenticated();
+  }
+
+  // Log out with redirection to home
+  logout() {
+    this.authService.logout();
+    this._router.navigate(['']).then(r => false);
   }
 
 }
