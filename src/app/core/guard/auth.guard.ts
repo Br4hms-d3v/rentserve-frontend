8ef70fc3fab9c9c48699f972631624da53d427e8
Brahms <bramso.brahms@gmail.com>
2025-07-14 00:50:04 +0200

    Add comment to explain how it works authGuard

--- src/app/core/guard/auth.guard.ts
+++ src/app/core/guard/auth.guard.ts
@@ -7,9 +7,11 @@ export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
+  // Allows to enter the application unless it is not connected
   if (authService.isAuthenticated()) {
     return true;
   } else {
+    // Redirect to home
     router.navigate(['']).then(r => false);
     return false;
   }
