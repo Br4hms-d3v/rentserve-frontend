b1ba11d3ee773b0e6d988861fae18d7bd61eb93a
Brahms <bramso.brahms@gmail.com>
2025-07-18 00:13:33 +0200

    Create a route for user(folder)

--- src/app/features/user/user.routes.ts
+++ src/app/features/user/user.routes.ts
@@ -4,6 +4,6 @@ import {authGuard} from '../../core/guard/auth.guard';
 import {ChangePasswordComponent} from './change-password/change-password.component';
 
 export const user_routes: Routes = [
-  {path: 'edit-profile', component: EditUserComponent, canActivate: [authGuard]},
+  {path: ':id/edit-profile', component: EditUserComponent, canActivate: [authGuard]},
   {path: 'edit-password', component: ChangePasswordComponent, canActivate: [authGuard]},
 ]
