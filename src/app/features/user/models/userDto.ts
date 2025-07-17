11cd97c0d88d13b421f89a81260d5443f4df5a03
Brahms <bramso.brahms@gmail.com>
2025-07-18 00:10:55 +0200

    Create a userDto to read data from user

---
+++ src/app/features/user/models/userDto.ts
@@ -0,0 +1,11 @@
+export interface UserDto {
+    id: number;
+    name: string;
+    firstName: string;
+    birthdate: Date;
+    pseudo: string;
+    email: string;
+    street: string;
+    city: string;
+    zipCode: string;
+}
