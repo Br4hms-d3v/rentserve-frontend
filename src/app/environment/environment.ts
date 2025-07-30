087a48aedd8867d8701c82e9039a49560a321f52
Brahms <bramso.brahms@gmail.com>
2025-07-30 19:06:32 +0200

    Environment for category

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -3,4 +3,5 @@ export const environment = {
   apiBaseUrl: 'http://localhost:8080',   // Base URL of the backend API
   authEndPoint: '/api/auth',  // Path for authentication routes
   userEndPoint: '/api/user', // Path for user routes
+  categoryEndPoint: '/api/categories', // Path for category routes
 }
