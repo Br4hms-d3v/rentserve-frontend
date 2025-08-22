75b9c210dbe4f66948f47283c8701f6e2051e0f7
Brahms <bramso.brahms@gmail.com>
2025-08-22 21:08:07 +0200

    add paginator with style scss

--- src/app/features/material/material-by-category/material-by-category.component.ts
+++ src/app/features/material/material-by-category/material-by-category.component.ts
@@ -4,10 +4,13 @@ import {MatButtonModule} from '@angular/material/button';
 import {MaterialService} from '../service/material.service';
 import {MaterialDto} from '../model/material';
 import {ActivatedRoute} from '@angular/router';
+import {MatPaginatorModule, PageEvent} from '@angular/material/paginator';
+import {ThemeService} from '../../../core/services/theme.service';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-material-by-category',
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MatPaginatorModule, NgClass],
   templateUrl: './material-by-category.component.html',
   styleUrl: './material-by-category.component.scss'
 })
@@ -18,18 +21,31 @@ export class MaterialByCategoryComponent implements OnInit {
   message = '';
   messageSuccess = '';
 
+  isDarkMode = false;
+
+  pagedMaterials: any[] = [];
+  pageSize = 25;
+  pageIndex = 0;
+
   constructor(
     private readonly _materialService: MaterialService,
     private readonly _route: ActivatedRoute,
+    private readonly _themeService: ThemeService,
   ) {
   }
 
   ngOnInit(): void {
+    this.getMaterialsByCategory();
+    this.getTheme();
+  }
+
+  getMaterialsByCategory() {
     this.nameOfCategory = this._route.snapshot.paramMap.get('nameOfCategory') ?? '';
 
     this._materialService.getMaterialsByCategory(this.nameOfCategory).subscribe({
       next: (data: MaterialDto[]) => {
         this.materialsByCategory = data;
+        this.paginatorMaterialByCategory();
       },
       error: (error) => {
         if (typeof error.error) {
@@ -42,6 +58,23 @@ export class MaterialByCategoryComponent implements OnInit {
       }
 
     })
+  }
+
+  private paginatorMaterialByCategory(){
+    const startIndex = this.pageIndex * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.pagedMaterials = this.materialsByCategory.slice(startIndex, endIndex);
+  }
+
+  onPageChange(event: PageEvent){
+    this.pageIndex = event.pageIndex;
+    this.pageSize = event.pageSize;
+    this.paginatorMaterialByCategory(); // ✅ recalcul des cartes visibles
+  }
+
+  getTheme(){
+    this.isDarkMode = this._themeService.isDarkMode(); // Get current theme (dark or light)
+    this._themeService.darkMode$.subscribe((mode: boolean) => this.isDarkMode = mode); // Watch changes in dark mode (reactive)
   }
 
 }
